27dfaf5410323e28c898440aea6c5df1b13a6e51
alexandre <alexandre.herrmann.67@gmail.com>
2024-03-11 08:36:26 +0100

    test

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -8,9 +8,9 @@ if [ -f /var/www/html/wp-config.php ]; then
 fi
 
 wp config create --allow-root --dbname=${DATABASE} --dbuser=${USER} --dbpass=${USER_PASSWORD} --dbhost=mariadb
-wp core install --allow-root --url=${HOSTNAME} --title="Inception" --admin_user=${ROOT_NAME} --admin_password=${ROOT_PASSWORD} --admin_email=${ROOT_NAME}.adm@${HOSTNAME}
+wp core install --allow-root --url=${HOSTNAME} --title="Inception" --admin_user=${ROOT_NAME} --admin_password=${ROOT_PASSWORD} --admin_email=${ROOT_NAME}.r@${HOSTNAME}
 wp user create --allow-root ${USER} ${USER}@${HOSTNAME} --role=author --user_pass=${USER_PASSWORD}
-wp theme activate --allow-root twentyfourteen
+wp theme activate --allow-root twentytwentytwo	
 wp plugin update --allow-root --all
 
 echo "done."
